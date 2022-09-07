9bcd4cb53075cc96656c3de8930f942d5de10836
Nichanok Auevechanichkul <nichanok.a@gmail.com>
2022-09-07 13:26:12 +0200

    add metrics od child pov and the chage some decirption

--- app.py
+++ app.py
@@ -12,6 +12,12 @@ df_final = pd.read_csv('fullcleaned.csv', dtype={'': str})
 '''
 # Youth in the City RESULTs
 '''
+st.write(f"Try to show the ugly facts about child poverty in Berlin")
+col1, col2, col3 = st.columns(3)
+col1.metric("Child poverty", "24.54%", "min: 0.66%, max:74.68%")
+col2.metric("Dynamic of child poverty (2018 to 2020)", "-1.04%", "min: -23.69%, max:18.42%")
+col3.metric("Gini-index", "0.38")
+
 def get_columnname(option, df):
     df_filtern = df[df.fullname == option]
     return df_filtern.column_name.values[0]
@@ -20,48 +26,41 @@ def get_fullname(column_name, df):
     return df_filtern.fullname.values
 
 option = st.selectbox('Which feature do you like to see on Berlin Maps?',
-     ('Migration in %',
+     ('Population with migration background in %',
  'Unemployment in %',
  'Welfare beneficiaries in %',
  'Child poverty in %',
- 'Dynamic of unemployment in %',
- 'Dynamic of welfare in %',
- 'Dynamic of child poverty in %',
- 'Rent in €/m2',
+ 'Dynamic of unemployment in % (2018 to 2020)',
+ 'Dynamic of welfare in % (2018 to 2020)',
+ 'Dynamic of child poverty in % (2018 to 2020)',
+ 'Average advertised rent in €/m2',
  'Social housing in %',
- 'The less-profit housing in %',
- 'Tranformation public to private appartment in %',
- 'Dynamic of public/private transformation  in %',
- 'Aparment sale in %',
- 'Dynamic of sales between 2015 and 2020',
- 'The people live in this area more than 5 years in %',
- 'Number of all public transports',
- 'Number of restaurants exc. fast food',
- 'Number of museums, art centers, cinemas, theaters, etc.',
- 'Number of ngos, social and community centers, etc.',
- 'Number of hospital, clinics, pharmacies, etc.',
- 'Number of fire station, police, post box, townhall, etc.',
- 'Number of langague and music schools, edu institutions',
- 'Number of schools',
- 'Number of universities ',
- 'Number of kindergartens',
- 'Number of picnic tables, benches, bbq, water points, etc.',
- 'Number of swimming pool, park, playground, etc.',
- 'Number of bars, pubcs, nightclubs, etc.',
- 'Number of lake, rivers, etc.',
- 'Building age older than 1940 in %',
- 'Building age between 1941-1991 in %',
- 'Building age between 1991-2001 in %',
- 'Green space in m2',
- 'Number of other types of schools',
- 'Number of vocational school',
- 'Number of primary school',
- 'Number of gymnasium',
- 'Number of other secondary school',
- 'Number of private school',
- 'Number of school for children with special needs',
- 'Number of kindergartens ',
- 'Number of rail / u-bahn / s-bahn, tram stations'))
+ 'Share of municipal housing companies in the housing stock in %',
+ 'Conversion of multi-family houses into condos in %',
+ 'Dynamic of condo conversion in % (2015 to 2020)',
+ 'Aparments sale in %',
+ 'Dynamic of apartment sales in % (2015 to 2020)',
+ 'Share of inhabitants with at least 5 years of residence in %',
+ 'Amount of public transport stops within 500 m, incl. bus',
+ 'Amount of restaurants and cafés within 500 m (exc. fast food)',
+ 'Amount of cultural institutions within 500 m (museums, cinemas, theaters, etc.)',
+ "Amount of extra-curriculum educational institutions within 500 m (music and language schools)'",
+ 'Amount of urban furniture (picnic tables, benches, bbq, water points, etc.) within 500 m',
+ 'Amount of places for outdoor leisure (swimming pools, parks, playgrounds, etc.) within 500 m',
+ 'Amount of bars, pubs, nightclubs, etc. within 500 m',
+ 'Share of houses built before 1940 in % (as of 2015)',
+ 'Share of houses built between 1941 and 1991 in % (as of 2015)',
+ 'Share of houses built between 1991 and 2001 in % (as of 2015)',
+ 'Vegetation volume in m3/m2',
+ 'Amount of other types of schools',
+ 'Amount of vocational schools',
+ 'Amount of primary schools',
+ 'Amount of Gymnasiums',
+ 'Amount of other secondary schools',
+ 'Amount of private schools',
+ 'Amount of schools for children with special needs',
+ 'Amount of kindergartens',
+ 'Amount of rail / U-bahn / S-bahn and tram stations'))
 
 st.write(f" Here is {option} in each Planningarea")
 
@@ -92,9 +91,9 @@ fig = px.choropleth_mapbox(
         opacity=0.5,
         #labels= {f"{color}: {color} amount"},
         hover_name='PLR_NAME',
-        hover_data={'PLR_ID':False, 'child_pov':True, color: True}
+        hover_data={'PLR_ID':False,'child_pov':True, 'BZR_NAME':True, color: True}
         )
-fig.update_layout(margin={"r": 0, "t": 0, "l": 0, "b": 0})
+fig.update_layout(margin={"r": 0, "t": 0, "l": 0, "b": 0}, coloraxis_colorbar= {'title':""})
 fig.show()
 
 st.plotly_chart(fig)
